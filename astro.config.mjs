d84ae4368671facf2adc3fe0aaf8f2a15190c7a1
Ian Ermicioi <ianermicioi14@gmail.com>
2026-05-12 11:28:42 +0300

    fix astro config with cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
   output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
